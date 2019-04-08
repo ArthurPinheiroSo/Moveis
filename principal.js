37431815f868c23abb08a7bb3ed3d052bcc40810
Aluno <Aluno>
2019-04-08 20:03:24 -0300

    script banner

---
+++ principal.js
@@ -0,0 +1,23 @@
+var banners = [
+ "A",
+ "-A",
+ "--A",
+ "----A",
+ "-------A",
+ "------------A",
+ "---------------------A",
+ "-------------A",
+ "--------A",
+ "-----A",
+ "--A",
+ "-A",
+ "A"
+ ];
+var bannerAtual=0;
+
+function TrocaBanner(){
+	bannerAtual = (bannerAtual + 1) % banners.length;
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+	}
+	
+setInterval(TrocaBanner , 100);
